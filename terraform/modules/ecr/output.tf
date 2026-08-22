e8e6d21fcedde738bcf0d4ebb613c8f1c4a0c50e
ioaviator <kingsolomonifeanyi@gmail.com>
2026-08-22 01:35:59 +0100

    fix(terraform): remove wrong usage of keyword

--- terraform/modules/ecr/output.tf
+++ terraform/modules/ecr/output.tf
@@ -1,4 +1,3 @@
 output "ecr_repo_url" {
-  type = string
   value = data.aws_ecr_repository.ticket_api.repository_url
 }
--- terraform/modules/iam/output.tf
+++ terraform/modules/iam/output.tf
@@ -1,9 +1,7 @@
 output "ecs_execution_role_arn" {
-  type = string
   value = aws_iam_role.ecs_task_execution_role.arn
 }
 
 output "ecs_task_role_arn" {
-  type = string
   value = aws_iam_role.ecs_task_role.arn
 }
