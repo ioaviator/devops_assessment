198ea92c910d39266e1d7c6a74824759c697bd02
ioaviator <kingsolomonifeanyi@gmail.com>
2026-08-23 17:28:28 +0100

    feat(terraform): push production rollout

--- terraform/modules/ecs/main.tf
+++ terraform/modules/ecs/main.tf
@@ -59,4 +59,13 @@ resource "aws_ecs_service" "ticket_api_service" {
     assign_public_ip = true
   }
 
+  deployment_circuit_breaker {
+    enable   = true
+    rollback = true
+  }
+
+  lifecycle {
+    ignore_changes = [task_definition]
+  }
+
 }
